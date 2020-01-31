fb368e946083362498bc4d67d2a26689c0f2af8a
matkomatko <matko.matko@hotmail.com>
2020-01-31 19:09:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/basics_of_web_and_mobile_application_development_project___android_part/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.basics_of_web_and_mobile_application_development_project___android_part;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
